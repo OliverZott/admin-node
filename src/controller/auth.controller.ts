0baab1d8665366417e2c66f10834928d8bbbc79a
Oliver Zott <zott_oliver@web.de>
2022-01-12 18:57:10 +0100

    Validation - validate json input

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,7 +1,18 @@
 import {Request, Response} from 'express';
+import {RegisterValidation} from "../validation/register.validation";
 
 
 export const register = (req: Request, res: Response) => {
-    res.send(req.body);     // response is same as request body
-    // res.send("Ressspoooonse ");
+    const body = req.body;
+    const {error} = RegisterValidation.validate(body);  // deconstruct validation result
+    if (error) {
+        return res.status(400).send(error.details);
+    }
+    if (body.password !== body.password_confirm) {
+        return res.status(400).send({
+            message: "Password confirmation must be the same."
+        });
+    }
+
+    res.send(body);     // response is the request body
 }
