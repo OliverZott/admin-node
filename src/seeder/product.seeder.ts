0cbdd2b7d86700f79a29a8ec122c6b19a1f96a86
Oliver Zott <zott_oliver@web.de>
2022-02-18 13:42:04 +0100

    Seeder for products added

---
+++ src/seeder/product.seeder.ts
@@ -0,0 +1,24 @@
+
+import { createConnection, getManager } from "typeorm";
+import { Product } from "../entity/product.entity";
+
+
+createConnection().then(async connection => {
+    console.log("Seeding products to the database.")
+    const repository = getManager().getRepository(Product);
+
+    const maxPrice = 50;
+    const minPrice = 1;
+
+    for (let i = 0; i < 30; i++) {
+        await repository.save({
+            title: `title-${i}`,
+            description: `description-${i}`,
+            image: `image${i}`,
+            price: (Math.random() * (maxPrice - minPrice) + minPrice),
+        })
+    };
+
+
+    process.exit(0);
+});
